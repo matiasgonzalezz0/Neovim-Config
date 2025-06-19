6c28f423281a49f98a1cecfd448dd2a274ba268d
matiasgonzalezz0 <matiasgonzalezz.mail@gmail.com>
2025-06-19 14:43:18 -0400

    Add: indentation user commands

--- lua/idk/set.lua
+++ lua/idk/set.lua
@@ -4,9 +4,6 @@ vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.mouse = "a"
@@ -34,3 +31,23 @@ vim.opt.hlsearch = true
 -- Disables netrw
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
+
+-- Modular function to set indentation
+local function set_indent(size)
+  vim.opt.tabstop = size
+  vim.opt.softtabstop = size
+  vim.opt.shiftwidth = size
+  print("Switched to " .. size .. "-space indentation")
+end
+
+-- Set default indentation
+set_indent(2)
+
+-- Create the commands
+vim.api.nvim_create_user_command('Indent2', function()
+  set_indent(2)
+end, {})
+
+vim.api.nvim_create_user_command('Indent4', function()
+  set_indent(4)
+end, {})
